f05076212565251705b5862c6cd12ee2bfee9cad
Roman Kempt <roman.kempt@tu-dresden.de>
2019-11-08 19:22:52 +0100

    packaged

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AIMS_tools-Roman-Kempt", # Replace with your own username
+    version="0.0.1",
+    author="Roman Kempt",
+    author_email="roman.kempt@tu-dresden.de",
+    description="A small toolbox to handle AIMS calculations.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='LGPLv3',
+    url="https://github.com/romankempt/AIMS_tools",
+    download_url="https://github.com/romankempt/AIMS_tools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+            'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Scientific/Engineering :: Chemistry',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=[
+        'ase',
+        'spglib',
+        'numpy',
+        'matplotlib',
+        'seaborn'
+    ],
+    python_requires='>=3.6',
+)
