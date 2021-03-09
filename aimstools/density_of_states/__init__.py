e51bbc92454afe74188d1980727fa73dc12b3992
Roman Kempt <roman.kempt@gmail.com>
2021-03-09 19:53:12 +0100

    preparing changes of cli utilities

--- aimstools/density_of_states/__init__.py
+++ aimstools/density_of_states/__init__.py
@@ -6,11 +6,13 @@ from aimstools.density_of_states.total_dos import TotalDOS
 from aimstools.density_of_states.atom_proj_dos import AtomProjectedDOS
 from aimstools.density_of_states.species_proj_dos import SpeciesProjectedDOS
 
+import os
+
 
 __all__ = ["DensityOfStates", "TotalDOS", "AtomProjectedDOS", "SpeciesProjectedDOS"]
 
-allow_plotting = os.environ.get("ALLOW_PLOTTING")
-if allow_plotting == False:
+allow_plotting = os.environ.get("AIMSOOLS_ALLOW_PLOTTING")
+if allow_plotting == "FALSE":
     raise Exception(
         "The plotting functionalities are not available on this system, e.g., if you are running aimstools on a cluster."
     )
