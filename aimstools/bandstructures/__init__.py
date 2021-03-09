a79f48ad14fe64fbf18d26d7216db22f1d0d9d05
Roman Kempt <roman.kempt@gmail.com>
2021-03-09 18:26:20 +0100

    Not allowing plotting on HPC systems

--- aimstools/bandstructures/__init__.py
+++ aimstools/bandstructures/__init__.py
@@ -6,10 +6,19 @@ from aimstools.bandstructures.regular_bandstructure import RegularBandStructure
 from aimstools.bandstructures.brillouinezone import BrillouineZone
 from aimstools.bandstructures.mulliken_bandstructure import MullikenBandStructure
 
+import os
+
 __all__ = [
     "BandStructure",
     "BrillouineZone",
     "MullikenBandStructure",
     "RegularBandStructure",
 ]
+
+
+allow_plotting = os.environ.get("AIMSOOLS_ALLOW_PLOTTING")
+if allow_plotting == False:
+    raise Exception(
+        "The plotting functionalities are not available on this system, e.g., if you are running aimstools on a cluster."
+    )
 
--- aimstools/density_of_states/__init__.py
+++ aimstools/density_of_states/__init__.py
@@ -7,10 +7,10 @@ from aimstools.density_of_states.atom_proj_dos import AtomProjectedDOS
 from aimstools.density_of_states.species_proj_dos import SpeciesProjectedDOS
 
 
-__all__ = [
-    "DensityOfStates",
-    "TotalDOS",
-    "AtomProjectedDOS",
-    "SpeciesProjectedDOS",
-]
+__all__ = ["DensityOfStates", "TotalDOS", "AtomProjectedDOS", "SpeciesProjectedDOS"]
 
+allow_plotting = os.environ.get("ALLOW_PLOTTING")
+if allow_plotting == False:
+    raise Exception(
+        "The plotting functionalities are not available on this system, e.g., if you are running aimstools on a cluster."
+    )
